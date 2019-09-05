7ca330788db97c9b55dce1360d087803e9b01b23
Aaron Watters <awatters@simonsfoundation.org>
2019-09-05 14:43:41 -0400

    delay redraw

--- sphere.py
+++ sphere.py
@@ -15,17 +15,18 @@ def sphere(N, radius=1.0, color="rgba(100, 100, 200, 0.5)", color2="rgba(100,100
     result.orbit_all(3)
     result.rotate_degrees((0,0,0), 3, (1, 1), 5)
     di = np.pi / N
-    for i in range(2 * N):
-        theta = di * i
-        for j in range(N):
-            c = color
-            if (i + j) % 3 == 0:
-                c = color2
-            phi = di * (j - N * 0.5)
-            triangle = [
-                sphere_point(theta, phi, radius),
-                sphere_point(theta + di, phi, radius),
-                sphere_point(theta, phi + di, radius)
-            ]
-            result.polygon(triangle, c)
+    with result.in_canvas.delay_redraw():
+        for i in range(2 * N):
+            theta = di * i
+            for j in range(N):
+                c = color
+                if (i + j) % 3 == 0:
+                    c = color2
+                phi = di * (j - N * 0.5)
+                triangle = [
+                    sphere_point(theta, phi, radius),
+                    sphere_point(theta + di, phi, radius),
+                    sphere_point(theta, phi + di, radius)
+                ]
+                result.polygon(triangle, c)
     return result
